eb66c4a422b244ccd93eeed933a5d437c68cd76d
David Sousa <58473448+DAVIDKSOUSA@users.noreply.github.com>
2023-09-12 18:50:42 -0300

    Update 4_📉_S&D.py

--- pages/4_📉_S&D.py
+++ pages/4_📉_S&D.py
@@ -124,6 +124,8 @@ if submit:
     # In[5]:
 
 
+
+
     
     df1["year"] = df1.index.year 
 
@@ -134,20 +136,13 @@ if submit:
     df1 = df1[df1['year'] == int(year) +1]
 
 
-
-   
-   
-    
-
-
-
     Upper_Band_12m1d = df1["Vol"][-1] * df1["Adj Close"][-1] + df1["Adj Close"][-1]
     Lower_Band_12m1d = df1["Adj Close"][-1] - df1["Vol"][-1] * df1["Adj Close"][-1]
 
     Upper_Band_12m2d = 2 * df1["Vol"][-1] * df1["Adj Close"][-1] + df1["Adj Close"][-1]
     Lower_Band_12m2d = df1["Adj Close"][-1] - 2 * df1["Vol"][-1] * df1["Adj Close"][-1]
 
-    Upper_Band_12m3d = 3 * df1["Vol"][-1] * df1[["Adj Close"][-1] + df1["Adj Close"][-1]
+    # Upper_Band_12m3d = 3 * df1["Vol"][-1] * df1[["Adj Close"][-1] + df1["Adj Close"][-1]
 
     # Lower_Band_12m4d = df1["Adj Close"][-1] - 4 * df1["Vol"][-1] * df1["Adj Close"][-1]
 
